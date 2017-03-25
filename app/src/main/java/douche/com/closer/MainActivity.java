f7d631281b7aa4c2eef5769e5520c5404dba3c28
Johnnylee <kfjohnny@ufrn.edu.br>
2017-03-25 00:28:12 -0300

    rssi base measure for beacon distance

--- app/src/main/java/douche/com/closer/MainActivity.java
+++ app/src/main/java/douche/com/closer/MainActivity.java
@@ -1,5 +1,8 @@
 package douche.com.closer;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -8,6 +11,7 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,6 +21,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -125,7 +130,22 @@ public class MainActivity extends AppCompatActivity {
             super.onScanResult(callbackType, result);
             mLeDeviceListAdapter.addDevice(result.getDevice(), result.getRssi());
             mLeDeviceListAdapter.notifyDataSetChanged();
+            if (result.getRssi() < -60) {
+                sendNotification("OUT OF SIGHT", "YOUR BEACON IS TOO FAR");
+            }
             swipe.setRefreshing(false);
         }
     };
-}git
+
+    public void sendNotification(String title, String content) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(getApplicationContext(), MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this).setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                        .setContentTitle(title)
+                        .setContentText(content)
+                        .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
+    }
+}
